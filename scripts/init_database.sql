93ca51c564fcffc6a07b26dbcc98d4583337cbc1
jatinkundra27 <jatinkundra27@gmail.com>
2025-12-05 23:46:52 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+-- create the 'DataWarehouse' database and schemas 'bronze','silver','gold'
+create database DataWarehouse;
+go
+
+use DataWarehouse;
+go
+
+
+-- create schemas
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
+
